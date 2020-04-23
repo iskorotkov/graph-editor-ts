e79e9570e909f4cef78c95bb6ba053c48c9bc207
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-04-23 14:21:51 +0500

    Add validation for parameters in constructor

--- src/graphs/content.ts
+++ src/graphs/content.ts
@@ -3,5 +3,8 @@ export class Content {
                 public text: string,
                 public author: string = 'Anonymous',
                 public creationDate: Date = new Date(Date.now())) {
+        if (creationDate > new Date(Date.now())) {
+            throw new RangeError('Creation date can\'t be in the future')
+        }
     }
 }
--- src/graphs/dimensions.ts
+++ src/graphs/dimensions.ts
@@ -1,4 +1,7 @@
 export class Dimensions {
     constructor(public height: number = 0, public width: number = 0) {
+        if (height < 0 || width < 0) {
+            throw new RangeError('Height or width can\'t be negative')
+        }
     }
 }
